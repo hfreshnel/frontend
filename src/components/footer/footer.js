19d44dfb5bd040e7084cabfa3171851fdcf74dab
Freshnel HOUENOU <fresh.kushimaru@gmail.com>
2025-01-17 01:46:41 +0100

    add footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+//footer with logo and social media links
+import React from 'react';
+import './footer.css';
+import logo from '../../assets/images/logo_2.png';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+        <div className="footer-logo">
+            <img src={logo} alt="Logo" />
+        </div>
+        </footer>
+    );
+}
+
+export default Footer;
