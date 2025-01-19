8890ec398261289636e4282ed6064a0aa40d4d0b
Grosbinator <roro.philis@gmail.com>
2025-01-19 14:24:01 +0100

    fiche client complète

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FicheKine from "./FicheKine";
+import FicheClient from "./FicheClient";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/fiche-kine" element={<FicheKine />} />
+                <Route path="/fiche-client" element={<FicheClient />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
