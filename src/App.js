94e0ad306c9dafa21f55abc478535b0b4a47a170
Grosbinator <roro.philis@gmail.com>
2025-01-19 15:45:29 +0100

    ajout d'une page principale

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FicheKine from "./FicheKine";
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import PagePrincipale from "./PagePrincipale";
 import FicheClient from "./FicheClient";
+import FicheKine from "./FicheKine";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/fiche-kine" element={<FicheKine />} />
-                <Route path="/fiche-client" element={<FicheClient />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        {/* Routes pour différentes pages */}
+        <Routes>
+          {/* Page principale */}
+          <Route path="/" element={<PagePrincipale />} />
+          {/* Pages FicheClient et FicheKine */}
+          <Route path="/fiche-client" element={<FicheClient />} />
+          <Route path="/fiche-kine" element={<FicheKine />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
