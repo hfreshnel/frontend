fb9426a34ee0ec1f11321c7dfe228f0a85e9cf2e
Freshnel HOUENOU <fresh.kushimaru@gmail.com>
2025-01-19 18:07:17 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-import Login from "./Login"; 
-import SignUp from "./register";
-import Dashboard from "./dashboard";
+import Login from "./pages/login/login";
+import SignUp from "./pages/register/register";
+import Dashboard from "./pages/dashboard/patient/dashboard";
 
 function App() {
   return (
@@ -10,8 +10,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<SignUp />} />
-            <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard route */}
-
+            <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
     </Router>
   );
