5e8b0ee0f5d193d91acc0cf3f7cffe28342529a0
Freshnel HOUENOU <fresh.kushimaru@gmail.com>
2025-02-06 10:24:43 +0100

    remove useless protected route

--- src/protected_routes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ element: Component, isAuthenticated, ...rest }) => {
-  return isAuthenticated ? <Component {...rest} /> : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
